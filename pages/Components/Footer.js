180f8d1f816ea9e4a748e4473d85991177100786
Yohannes Lemma <johnlemma9@gmail.com>
2023-03-01 16:58:13 +0300

    footer

--- pages/Components/Footer.js
+++ pages/Components/Footer.js
@@ -4,7 +4,7 @@ import NavBar from './NavBar'
 
 function Footer() {
   return (
-    <footer className=' bg-slate-100 p-10 font-burtons flex justify-between'>
+    <footer className=' bg-slate-100 p-10 font-burtons md:flex gap-5 justify-between '>
         <div>
             <h3>Email:johnlemma9@gmail.com</h3>
             <h3>phone-Number:+251973374428</h3>
