775fd382f18937854aed768b5609dbad76a86846
Yohannes Lemma <johnlemma9@gmail.com>
2023-03-01 16:52:32 +0300

    cv link

--- pages/Components/NavBar.js
+++ pages/Components/NavBar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
+// import cv from '../../public/Yohannes_Lemma_CV.pdf'
+
 import Logo from './Logo'
 export default function NavBar({DarkMode,setDarkMode}) {
   return (
@@ -8,7 +10,11 @@ export default function NavBar({DarkMode,setDarkMode}) {
       <ul className='flex md:items-center'>
         <li><BsFillMoonStarsFill onClick={()=>{setDarkMode(!DarkMode)}} className=' cursor-pointer text-2xl'/></li>
         <li>
-          <a className=' bg-gradient-to-r from-cyan-500 p-2 to-teal-500 py-1 border-none rounded ml-3   md:block ' href='#'>My-Resume</a>
+          <a className=' bg-gradient-to-r from-cyan-500 p-2 to-teal-500 py-1 border-none rounded ml-3   md:block '     href="/Yohannes_Lemma_CV.pdf"
+    alt="alt text"
+    target="_blank"
+    rel="noopener noreferrer"
+ download>My-Resume</a>
         </li>
       </ul>
     </nav>
